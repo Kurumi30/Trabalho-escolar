122ceb97f17b6aaa84ee5d281ff7d711aa3f1ddd
Kurumi30 <bsd3su@gmail.com>
2022-09-20 02:16:55 -0300

    Quase finalizado...

--- script.js
+++ script.js
@@ -1,61 +1,25 @@
-function TestaCPF(strCPF) {
-    var Soma;
-    var Resto;
-    Soma = 0;
-  if (strCPF == "00000000000") return false;
+//const { validate } = require('cpf-check')
 
-  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
-  Resto = (Soma * 10) % 11;
+let button = document.getElementById('button');
+button.addEventListener('click', function click() {
+    button.style.background = 'purple'
+})
 
-    if ((Resto == 10) || (Resto == 11))  Resto = 0;
-    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+function redirect() {
+	let cpf = document.getElementsByClassName('pass-input');
 
-  Soma = 0;
-    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
-    Resto = (Soma * 10) % 11;
+    let username = document.getElementsByClassName('user-input');
+    let password = document.getElementsByClassName('pass-input');
 
-    if ((Resto == 10) || (Resto == 11))  Resto = 0;
-    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
-    return true;
+	if(username[0].value) {
+		if(password[0].value == "1234567890" /*|| validate(cpf) == true*/) {
+			alert("Seja bem vindo!");
+			confirm("Esse é o meu projeto! Espero que goste, professor(a) " + username[0].value + "!");
+			window.location.href = "./HTML/index2.html";
+		} else {
+			alert("Usuário ou senha incorretos, por favor tente novamente!");
+		}
+	}
+
 }
-var strCPF = "1234567890";
-console.log(TestaCPF(strCPF));
-
-//Outro código
-
-function validarCPF(cpf) {	
-	cpf = cpf.replace(/[^\d]+/g,'');	
-	if(cpf == '') return false;	
-	// Elimina CPFs invalidos conhecidos	
-	if (cpf.length != 11 || 
-		cpf == "00000000000" || 
-		cpf == "11111111111" || 
-		cpf == "22222222222" || 
-		cpf == "33333333333" || 
-		cpf == "44444444444" || 
-		cpf == "55555555555" || 
-		cpf == "66666666666" || 
-		cpf == "77777777777" || 
-		cpf == "88888888888" || 
-		cpf == "99999999999")
-			return false;		
-	// Valida 1o digito	
-	add = 0;	
-	for (i=0; i < 9; i ++)		
-		add += parseInt(cpf.charAt(i)) * (10 - i);	
-		rev = 11 - (add % 11);	
-		if (rev == 10 || rev == 11)		
-			rev = 0;	
-		if (rev != parseInt(cpf.charAt(9)))		
-			return false;		
-	// Valida 2o digito	
-	add = 0;	
-	for (i = 0; i < 10; i ++)		
-		add += parseInt(cpf.charAt(i)) * (11 - i);	
-	rev = 11 - (add % 11);	
-	if (rev == 10 || rev == 11)	
-		rev = 0;	
-	if (rev != parseInt(cpf.charAt(10)))
-		return false;		
-	return true;   
-}
+redirect()
